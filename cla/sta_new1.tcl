95b8775a8b0e74204d02f6ef6c9756b4e7ed3b0c
Vision-VLSI Industry Training Institute <vvi.pvtltd@gmail.com>
2024-02-08 13:53:45 +0530

    Create sta_new1.tcl

---
+++ cla/sta_new1.tcl
@@ -0,0 +1,35 @@
+create_clock -period 100 -name virtual_clock
+set_input_delay 0 -min -rise [get_ports a*] -clock virtual_clock
+set_input_delay 0 -min -fall [get_ports a*] -clock virtual_clock
+set_input_delay 0 -max -rise [get_ports a*] -clock virtual_clock
+set_input_delay 0 -max -fall [get_ports a*] -clock virtual_clock
+set_input_transition 1.5 -min -rise [get_ports a*] 
+set_input_transition 1.5 -min -fall [get_ports a*] 
+set_input_transition 1.5 -max -rise [get_ports a*] 
+set_input_transition 1.5 -max -fall [get_ports a*] 
+set_input_delay 0 -min -rise [get_ports b*] -clock virtual_clock
+set_input_delay 0 -min -fall [get_ports b*] -clock virtual_clock
+set_input_delay 0 -max -rise [get_ports b*] -clock virtual_clock
+set_input_delay 0 -max -fall [get_ports b*] -clock virtual_clock
+set_input_transition 1.5 -min -rise [get_ports b*] 
+set_input_transition 1.5 -min -fall [get_ports b*] 
+set_input_transition 1.5 -max -rise [get_ports b*] 
+set_input_transition 1.5 -max -fall [get_ports b*] 
+set_input_delay 0 -min -rise [get_ports cin*] -clock virtual_clock
+set_input_delay 0 -min -fall [get_ports cin*] -clock virtual_clock
+set_input_delay 0 -max -rise [get_ports cin*] -clock virtual_clock
+set_input_delay 0 -max -fall [get_ports cin*] -clock virtual_clock
+set_input_transition 1.5 -min -rise [get_ports cin*] 
+set_input_transition 1.5 -min -fall [get_ports cin*] 
+set_input_transition 1.5 -max -rise [get_ports cin*] 
+set_input_transition 1.5 -max -fall [get_ports cin*] 
+set_output_delay -9 -min -rise [get_ports sum*] -clock virtual_clock
+set_output_delay -9 -min -fall [get_ports sum*] -clock virtual_clock
+set_output_delay 40 -max -rise [get_ports sum*] -clock virtual_clock
+set_output_delay 40 -max -fall [get_ports sum*] -clock virtual_clock
+set_load -pin_load 4 [get_ports sum*]
+set_output_delay -9 -min -rise [get_ports co*] -clock virtual_clock
+set_output_delay -9 -min -fall [get_ports co*] -clock virtual_clock
+set_output_delay 40 -max -rise [get_ports co*] -clock virtual_clock
+set_output_delay 40 -max -fall [get_ports co*] -clock virtual_clock
+set_load -pin_load 4 [get_ports co*]
